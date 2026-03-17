d9e7a71383069a3ba8921eab3a152162ae083942
Eze Mario <ezemario90@gmail.com>
2026-03-17 00:59:08 -0300

    fix: agregar useEffect al import en profile.tsx

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MainLayout from '../src/layout/MainLayout';
 
 const ProfilePage: React.FC = () => {
