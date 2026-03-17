67fdae3440fb16bc2de66b08221fc80902d58297
Eze Mario <ezemario90@gmail.com>
2026-03-17 00:02:58 -0300

    fix: tipado NextApiRequest y NextApiResponse en handler connections.ts

--- frontend/pages/api/connections.ts
+++ frontend/pages/api/connections.ts
@@ -1,9 +1,13 @@
 import { getServerSession } from "next-auth/next";
 import authOptions from "./auth/[...nextauth]";
-
 import type { Session } from "next-auth";
+import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../src/lib/prisma";
 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const session = await getServerSession(req, res, authOptions) as Session | null;
   if (!session?.user?.id) return res.status(401).json({ error: "No autenticado" });
 
