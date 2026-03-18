e535ea933dbd76fade15d2149cdfa646545482b9
Eze Mario <ezemario90@gmail.com>
2026-03-18 01:31:19 -0300

    fix: eliminar importación duplicada de prisma en connections.ts

--- frontend/pages/api/connections.ts
+++ frontend/pages/api/connections.ts
@@ -12,8 +12,6 @@ import type { Session } from "next-auth";
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "./lib/prisma";
 
-import prisma from "./lib/prisma";
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
