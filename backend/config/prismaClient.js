d5fc8649f7437c368a45d276acb280eff966e6c1
Eze Mario <ezemario90@gmail.com>
2026-04-06 19:50:07 -0300

    fix: usar adapter en PrismaClient para Prisma 7

--- backend/config/prismaClient.js
+++ backend/config/prismaClient.js
@@ -1,9 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient({
-	datasource: {
-		url: process.env.DATABASE_URL,
-	},
+	adapter: process.env.DATABASE_URL,
 });
 
 module.exports = prisma;
